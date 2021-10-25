054eb7ed0bb9ce838508ee2dc0596691e21d480f
emilousim <emilousim@gmail.com>
2021-10-24 23:58:12 -0400

    assessment 2

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// function inArray(needle, haystack) {
+//     var length = haystack.length;
+//     for (var i = 0; i < length; i++) {
+//     if (haystack[i] == needle)
+//      return true;
+//     }
+//     return false;
+//    }
